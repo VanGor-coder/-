45d145fd84e006fe92792fa50a5ab81f574a8aa3
gaofan <565948922@qq.com>
2021-01-04 19:39:46 +0800

    个人详情页(代码)

---
+++ 个人详情页(纯代码)/UITextField+TXF.h
@@ -0,0 +1,18 @@
+//
+//  UITextField+TXF.h
+//  个人详情页(纯代码)
+//
+//  Created by GaoFan on 2020/12/21.
+//  Copyright © 2020 GaoFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITextField (TXF)
+//@property (nonatomic,strong )UITextField *tf;
+-(void)addInputAccessoryView;
+@end
+
+NS_ASSUME_NONNULL_END
